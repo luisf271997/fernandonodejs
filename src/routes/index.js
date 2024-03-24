030a386e4eba3376bf7dd431969b6e7c2bd18f25
luis fernando <luferpaniagua27@gmail.com>
2024-03-24 15:02:42 -0500

    nodejs

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import { Router} from "express"
+const router = Router()
+
+router.get("/",(req,res)=>res.render('index', {title: 'este es el index'}))
+router.get("/about",(req,res)=>res.render('about', {title: 'este es el about'}))
+router.get("/contact",(req,res)=>res.render('contact', {title: 'este es el contact'}))
+
+export default router
